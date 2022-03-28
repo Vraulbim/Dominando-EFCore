d5e40397d5cff00364296e5d2c16e55b0b7bfc08
Vitor Raul Bim <vitorrb36@gmail.com>
2022-03-28 12:22:45 -0300

    Projeto Finalizado

---
+++ M15 - Migracoes/Curso/src/Domain/Pessoa.cs
@@ -0,0 +1,10 @@
+namespace src.Domain
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public int Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Documento { get; set; }
+    }
+}
---
+++ M15 - Migracoes/Curso/src/Migrations/20211109131215_AdicionarDocumento.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCore.Migrations
+{
+    public partial class AdicionarDocumento : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Documento",
+                table: "Pessoas",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Documento",
+                table: "Pessoas");
+        }
+    }
+}
